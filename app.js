3a03be74a840f0c625f7e158ffa2df4a79216576
sanjaymoglix <sanjay.kumar@moglix.com>
2023-11-13 23:50:11 +0530

    lesson 3 is completed

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const { ObjectId } = require('mongodb')
 const { connectToDb, getDb } = require('./db')
 
 // init app & middleware
@@ -29,6 +30,21 @@ app.get('/books', (req, res) => {
 
         })
         .catch((err) => {
-            res.status(500).json({error: "Could not fetch the documents"})
+            res.status(500).json({ error: "Could not fetch the documents" })
         })
+})
+
+app.get('/books/:id', (req, res) => {
+    if (ObjectId.isValid(req.params.id)) {
+        db.collection('books')
+            .findOne({ _id: new ObjectId(req.params.id) })
+            .then((doc) => {
+                res.status(200).json(doc)
+            })
+            .catch((err) => {
+                res.status(500).json({ error: "Could not fetch the document" })
+            })
+    } else {
+        res.status(500).json({ error: "Not a valid document id" })
+    }
 })
