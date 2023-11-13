2bbbad2fc9228dc48a988c209b15b2810955642d
sanjaymoglix <sanjay.kumar@moglix.com>
2023-11-13 23:06:41 +0530

    completed lesson 2

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
-const express = require('express');
+const express = require('express')
+const { connectToDb, getDb } = require('./db')
 
 // init app & middleware
-const app = express();
+const app = express()
 
-app.listen(3000, () => {
-    console.log('app listining on port 3000');
+// db connetion
+let db
+
+connectToDb((err) => {
+    if (!err) {
+        app.listen(3000, () => {
+            console.log('app listining on port 3000')
+        })
+        db = getDb()
+    }
 })
 
 // routes
 app.get('/books', (req, res) => {
-    res.json({msg: 'welcome to the api'});
+    let books = [];
+
+    db.collection('books')
+        .find()
+        .sort({ author: 1 })
+        .forEach((book) => books.push(book))
+        .then(() => {
+            res.status(200).json(books)
+
+        })
+        .catch((err) => {
+            res.status(500).json({error: "Could not fetch the documents"})
+        })
 })
